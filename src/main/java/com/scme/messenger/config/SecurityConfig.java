b91a3345639f29213346dc8fe468107592302229
Hashem-imposter1822 <72875896+HashemGhanim@users.noreply.github.com>
2024-04-08 15:10:19 +0200

    Add CORS origin allow

--- src/main/java/com/scme/messenger/config/SecurityConfig.java
+++ src/main/java/com/scme/messenger/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.scme.messenger.filter.JwtAuthenticationFilter;
 
@@ -55,4 +57,16 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
 }
