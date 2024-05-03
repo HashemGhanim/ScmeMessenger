9e8518c311a1f6e5e7d82c1e32b6946a15d04142
Hashem-imposter1822 <72875896+HashemGhanim@users.noreply.github.com>
2024-05-03 16:17:30 +0300

    Add a GET All User Chats

--- src/main/java/com/scme/messenger/config/DataInitializer.java
+++ src/main/java/com/scme/messenger/config/DataInitializer.java
@@ -22,22 +22,44 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user1 = User.builder()
                 .email("hashemzerei45@gmail.com")
-                .name("hashem")
+                .name("Hashem")
                 .userId("20201101")
+                .password(passwordEncoder.encode("Hh@123456"))
                 .role(Role.STUDENT)
+                .registered(true)
                 .build();
 
         User user2 = User.builder()
                 .email("moodghoz@gmail.com")
-                .name("Mahmmod")
+                .name("Mahmmoud")
                 .userId("20201102")
                 .password(passwordEncoder.encode("Hh@123456"))
                 .role(Role.ADMIN)
                 .registered(true)
                 .build();
 
+        User user3 = User.builder()
+                .email("salmanzerei45@gmail.com")
+                .name("Salman")
+                .userId("20201103")
+                .password(passwordEncoder.encode("Hh@123456"))
+                .role(Role.STUDENT)
+                .registered(true)
+                .build();
+
+        User user4 = User.builder()
+                .email("hasheeem1712@gmail.com")
+                .name("Sami")
+                .userId("20201104")
+                .password(passwordEncoder.encode("Hh@123456"))
+                .role(Role.STUDENT)
+                .registered(true)
+                .build();
+
         userRepo.save(user1);
         userRepo.save(user2);
+        userRepo.save(user3);
+        userRepo.save(user4);
     }
 
 }
